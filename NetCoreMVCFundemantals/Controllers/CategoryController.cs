f31e190610fd3e92adab9f4c087f76dd267a9eac
mert.alptekin <mert.alptekin@neomina.com>
2023-04-24 12:44:24 +0300

    Add project files.

---
+++ NetCoreMVCFundemantals/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCFundemantals.Models;
+
+namespace NetCoreMVCFundemantals.Controllers
+{
+  public class CategoryController : Controller
+  {
+
+    [HttpGet("kategoriler")]
+    public IActionResult Index()
+    
+    {
+      // model oluşturduk
+      var clist = new List<CategoryViewModel>();
+      clist.Add(new CategoryViewModel() { Name = "Kategori1", Description = "Açıklama1" });
+      clist.Add(new CategoryViewModel { Name = "Kategori1", Description = "Açıklama2" });
+
+
+      // view model gönderdik.
+      // view'e sadece 1 model gönderibilir. 
+      // bu durumda view başka model nasıl taşayacağız ?
+
+      return View(clist);
+    }
+  }
+}
---
+++ NetCoreMVCFundemantals/Models/CategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace NetCoreMVCFundemantals.Models
+{
+  public class CategoryViewModel
+  {
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+
+
+    public CategoryViewModel()
+    {
+      Id = Guid.NewGuid().ToString();
+    }
+
+
+  }
+}
